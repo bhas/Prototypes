ea0fa34554d6bf2cb31685e0cfbaf215a92f96ea
Bjorn Vinther <g.bvinther@gmail.com>
2017-04-02 02:13:50 +0200

    paper robot template

---
+++ Assets/Editor/FbxImportSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FbxImporter : AssetPostprocessor
+{
+    void OnPreprocessModel()
+    {
+        var importer = assetImporter as ModelImporter;
+        var name = importer.assetPath.ToLower();
+        if (name.Substring(name.Length - 4, 4) == ".fbx")
+        {
+            importer.globalScale = 10000 * 500;
+            importer.generateAnimations = ModelImporterGenerateAnimations.None;
+            importer.importMaterials = false;
+            importer.importAnimation = false;
+            importer.importBlendShapes = false;
+            importer.importTangents = ModelImporterTangents.None;
+        }
+    }
+}
